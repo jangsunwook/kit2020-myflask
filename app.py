81c4af903ebdd8b6b8996bac13cfab4d4ff93544
jangsunwook <20191008@g.ac.kr>
2020-06-07 04:25:16 +0900

    필요 없는 부분 주석 처리

--- app.py
+++ app.py
@@ -97,6 +97,7 @@ def method():
     return 'Hello, World!'    
 
 if __name__ == '__main__':
-    with app.test_request_context():
-        print(url_for('daum'))
+    #with app.test_request_context():
+        #print(url_for('daum'))
+        
     app.run(debug=True)   
