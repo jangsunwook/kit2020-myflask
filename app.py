6d80630e9ce3dc7ade50f6a1c2c44ab075f1ba7e
jangsunwook <20191008@g.ac.kr>
2020-06-22 18:00:36 +0900

    부트스트랩 디자인 적용

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template , redirect, url_for, abort
+from flask import Flask, request, render_template , redirect, url_for, abort , session
 
 import game
 import json
@@ -7,9 +7,11 @@ import dbdb
 
 app = Flask(__name__)
 
+app.secret_key = b'aaa!111/'
+
 @app.route('/')
 def index():
-    return '메인페이지'
+    return render_template('main.html')
 
 
 @app.route('/hello')
@@ -95,11 +97,41 @@ def login():
         print(pw,type(pw))
         #id와pw가 임의로 db 값이랑 비교 해서 맞으면 맞다 틀리면 틀리다
         ret = dbdb.select_user(id, pw)
+        print(ret[2])
         if ret != None:
-            return "안녕하세요~ {} 님" .format(id)
+            session['user'] = id
+            return redirect(url_for('index'))
         else:
-            return "아이디 또는 패스워드를 확인 하세요."    
+            return redirect(url_for('login')) 
 
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+# 회원가입
+            
+@app.route('/join', methods=['GET', 'POST'])
+def join():
+    if request.method == 'GET':
+        return render_template('join.html')
+    else:
+        id = request.form['id']   
+        pw = request.form['pw'] 
+        name = request.form['name'] 
+        print(id,type(id))
+        print(pw,type(pw))
+        ret = dbdb.check_id(id)
+        if ret != None:
+            return '''
+                    <script>
+                    alert('다른 아이디를 사용하세요');
+                    location.href='/join';
+                    </script>
+                '''
+        #id와pw가 임의로 db 값이랑 비교 해서 맞으면 맞다 틀리면 틀리다
+        dbdb.insert_user(id, pw, name)
+        return redirect(url_for('login'))
         
 
 @app.route('/form')
@@ -120,9 +152,12 @@ def method():
 
 @app.route('/getinfo')
 def getinfo():
-    ret = dbdb.select_all()
-    print(ret[3])
-    return render_template('getinfo.html', data=ret)
+    if 'user' in session:
+        ret = dbdb.select_all()
+        print(ret[3])
+        return render_template('getinfo.html', data=ret)
+
+    return redirect(url_for('login'))
     # return '번호 : {}, 이름 : {}'.format(student[ret[0], ret[1])
     
 if __name__ == '__main__':
